bee7a1a1ac7a053fdba9848dbc1d5a2880526656
samiksha275 <agarwalsamiksha88@gmail.com>
2025-11-25 02:01:30 +0530

    Basic structure of controllers added

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,3 +1,23 @@
-const register = async (req, res) => {
-    
+const registerUserAcc = async (req, res) => {
+    try {
+        const {email, password, phone, name, idtype, idproofnum} = req.body;
+        if(!email || !password || !phone || ! name || !idtype || !idproofnum)
+            return res.status(400).json({status: false, message: "Missing details"})
+        return res.status(200).json({status: true}) 
+    } catch (err) {
+        console.log(err.message)
+        return res.status(500).json({status: false, message: "Internal server error"})
+    }
+}
+
+const registeraccdetails = async(req, res) => {
+    try{
+        const {comname, bustype, cin, gstin, iec, apeda, fssai, pan, comemail, comphone, address, state, district, pincode} = req.body;
+        if(!comname, !bustype || !gstin || !iec || !pan || !comemail || !comphone || !address || !state || !district || !pincode)
+            return res.status(400).json({status: false, message: "Missing details"})
+        return res.status(200).json({status: true})
+    } catch (err) {
+        console.log(err.message)
+        return res.status(500).json({status: false, message: "Internal server error"})
+    }
 }
